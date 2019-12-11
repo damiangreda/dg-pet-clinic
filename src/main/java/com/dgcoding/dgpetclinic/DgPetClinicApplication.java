b27cd79dba94e20b8bb440024b92dd307c6c8ce4
damian@skyevent.pl <trabid@gmail.com>
2019-12-11 21:02:26 +0100

    init project add

---
+++ src/main/java/com/dgcoding/dgpetclinic/DgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.dgcoding.dgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DgPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dgcoding/dgpetclinic/DgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgcoding.dgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
